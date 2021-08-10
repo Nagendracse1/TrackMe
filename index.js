4236537b59f06e4cd98a1c94836c56ccd525df10
Nagendra Kumar <nagendrakumar@Nagendras-MacBook-Air.local>
2021-08-10 11:15:37 +0530

    UI added

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const { config, engine } = require('express-edge');
+config({ cache: process.env.NODE_ENV === 'production' });
+
+
+const app = new express();
+
+
+app.use(express.static('public'));
+app.use(engine);
+
+app.set('views',`${__dirname}/views`);
+
+
+const homePageController = require('./controllers/homePage');
+
+
+app.get('/',homePageController);
+
+
+app.listen(1007,()=>{
+
+    console.log("App listen on Port 1007");
+});
