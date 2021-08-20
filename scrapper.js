414b4180b7db4aa94e0d5c9d7138b575da8c3c9b
Nagendra Kumar <40954241+Nagendracse1@users.noreply.github.com>
2021-08-20 10:57:17 +0530

    loader added

--- scrapper.js
+++ scrapper.js
@@ -36,21 +36,21 @@ const cheerio = require('cheerio');
 const request = require('request');
 
 
-request({url: encodeURI('https://www.amazon.in/dp/B08LRDM44F/ref=s9_acsd_al_bw_c2_x_0_i?pf_rd_m=A1K21FY43GMZF8&pf_rd_s=merchandised-search-4&pf_rd_r=66JQV68N067ZXDD8F4WT&pf_rd_t=101&pf_rd_p=0d9bd1b8-0a59-4839-a3f9-38a902ab3e28&pf_rd_i=21634722031'),headers:{'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:80.0) Gecko/20100101 Firefox/80.0'} }, function (error, response, body) {
-  console.log('-----amazon----');
-  console.error('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // console.log('body:', body); // Print the HTML for the Google homepage.
-  fs.writeFileSync('try.txt',body);
-  const $ =cheerio.load(body);
-    // console.log(response.body);
-    console.log('-------');
-    fs.writeFileSync('try.txt',response.body);
-    const price = $('#priceblock_ourprice').text();
-    const productName = $('#productTitle').text().trim();
-    const available = $('#availability > span').text().trim();
-    console.log(price, "\n",productName, "\n",available, "\n"); 
-});
+// request({url: encodeURI('https://www.amazon.in/dp/B08LRDM44F/ref=s9_acsd_al_bw_c2_x_0_i?pf_rd_m=A1K21FY43GMZF8&pf_rd_s=merchandised-search-4&pf_rd_r=66JQV68N067ZXDD8F4WT&pf_rd_t=101&pf_rd_p=0d9bd1b8-0a59-4839-a3f9-38a902ab3e28&pf_rd_i=21634722031'),headers:{'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:80.0) Gecko/20100101 Firefox/80.0'} }, function (error, response, body) {
+//   console.log('-----amazon----');
+//   console.error('error:', error); // Print the error if one occurred
+//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//   // console.log('body:', body); // Print the HTML for the Google homepage.
+//   fs.writeFileSync('try.txt',body);
+//   const $ =cheerio.load(body);
+//     // console.log(response.body);
+//     console.log('-------');
+//     fs.writeFileSync('try.txt',response.body);
+//     const price = $('#priceblock_ourprice').text();
+//     const productName = $('#productTitle').text().trim();
+//     const available = $('#availability > span').text().trim();
+//     console.log(price, "\n",productName, "\n",available, "\n"); 
+// });
 
 
 // request('https://www.flipkart.com/harvard-full-sleeve-solid-men-jacket/p/itmed3a778baf007?pid=JCKFHHRNH6PYWRED&lid=LSTJCKFHHRNH6PYWREDDQ6ZVH', function (error, response, body) {
@@ -68,24 +68,24 @@ request({url: encodeURI('https://www.amazon.in/dp/B08LRDM44F/ref=s9_acsd_al_bw_c
 //     const available = $('#container > div > div._2c7YLP.UtUXW0._6t1WkM._3HqJxg > div._1YokD2._2GoDe3 > div._1YokD2._3Mn1Gg.col-8-12 > div:nth-child(2) > div > div._2JC05C').text().trim();
 //     console.log(price, "\n", productName, "\n", available, "\n"); 
 // });
-
-// request('https://www.myntra.com/mobile-stand', function (error, response, body) {
-//   console.log('-----myntra----');
-//   console.error('error:', error); // Print the error if one occurred
-//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-//   // console.log('body:', body); // Print the HTML for the Google homepage.
-//   fs.writeFileSync('try.txt',body);
-//   const $ =cheerio.load(body);
-//     // console.log(response.body);
-//     console.log('-------');
-//     // fs.writeFileSync('try.txt',response.body);
-//     //#mountRoot > div > div > div > main > div.pdp-details.common-clearfix > div.pdp-description-container > div.pdp-price-info > p.pdp-discount-container > span > strong
-//     const price = $('#mountRoot > div > div > div > main > div.pdp-details.common-clearfix > div.pdp-description-container > div.pdp-price-info > p.pdp-discount-container > span.pdp-price > strong').text();
-//     const productName = $('#mountRoot > div > div > div > main > div.pdp-details.common-clearfix > div.pdp-description-container > div.pdp-price-info > h1.pdp-name').text().trim();
-//     //const available = $('#mountRoot > div > div > div > main > div.pdp-details.common-clearfix > div.pdp-description-container > div:nth-child(3) > div > div.pdp-add-to-bag.pdp-button.pdp-flex.pdp-center.pdp-out-of-stock').text().trim();
-//     //console.log(price, "\n", productName, "\n", available, "\n")
-//     console.log(price, "\n", productName, "\n")
-// });
+console.log('-----')
+  request({url:'https://www.myntra.com/mobile-accessories/nikkispride/marble-mobile-stand/13404382/buy',headers:{'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'},verify=False}, function (error, response, body) {
+  console.log('-----myntra----');
+  console.error('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  // console.log('body:', body); // Print the HTML for the Google homepage.
+  fs.writeFileSync('try.txt',body);
+  const $ =cheerio.load(body);
+    // console.log(response.body);
+    console.log('-------');
+    // fs.writeFileSync('try.txt',response.body);
+    //#mountRoot > div > div > div > main > div.pdp-details.common-clearfix > div.pdp-description-container > div.pdp-price-info > p.pdp-discount-container > span > strong
+    const price = $('#mountRoot > div > div > div > main > div.pdp-details.common-clearfix > div.pdp-description-container > div.pdp-price-info > p.pdp-discount-container > span.pdp-price > strong').text();
+    const productName = $('#mountRoot > div > div > div > main > div.pdp-details.common-clearfix > div.pdp-description-container > div.pdp-price-info > h1.pdp-name').text().trim();
+    //const available = $('#mountRoot > div > div > div > main > div.pdp-details.common-clearfix > div.pdp-description-container > div:nth-child(3) > div > div.pdp-add-to-bag.pdp-button.pdp-flex.pdp-center.pdp-out-of-stock').text().trim();
+    //console.log(price, "\n", productName, "\n", available, "\n")
+    console.log(price, "\n", productName, "\n")
+});
 
 // request('https://www.nykaa.com/savlon-disinfectant-liquid/p/262959?pps=2&productId=262959&ptype=product&root=cav_pd&skuId=262957', function (error, response, body) {
 //   console.log('-----nykaa----');
