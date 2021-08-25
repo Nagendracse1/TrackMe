4a9dedbd88a8b3bfe8d78f7d435dc83e0aa8a1b3
Nagendra Kumar <40954241+Nagendracse1@users.noreply.github.com>
2021-08-25 12:02:44 +0530

    config

--- config/apiKey.js
+++
@@ -1,4 +0,0 @@
-
-module.exports = ()=>{
-    return "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" ;
-}
--- config/db.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-
-    getDbConnectionString: function(){
-        return 'mongodb+srv://nagendra:huiHPhMvXmq2Kn@trackme.21hj3.mongodb.net/TrackMe?retryWrites=true&w=majority';
-    }
-}
-
-
-
