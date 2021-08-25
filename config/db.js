2132ef22b27a8627899a54b708b2906913825e12
Nagendra Kumar <40954241+Nagendracse1@users.noreply.github.com>
2021-08-25 12:23:03 +0530

    ignore update

---
+++ config/db.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+    getDbConnectionString: function(){
+        return 'mongodb+srv://nagendra:huiHPhMvXmq2Kn@trackme.21hj3.mongodb.net/TrackMe?retryWrites=true&w=majority';
+    }
+}
+
+
+
+
